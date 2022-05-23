0d9de80f808edbe9d895024cee2dd18f942a8732
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-05-23 22:36:22 +0200

    feat(i18n): add language switch

--- components/Explaination.vue
+++ components/Explaination.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="text-2xl text-900 font-bold mb-4">{{ $t('explanation.headline') }}</div>
       <template v-for="(item, idx) in content">
-        <div class="grid mt-3" :key="idx">
+        <div class="grid mt-3" :key="`${idx}-el`">
           <div class="col-12 text-900 lg:col-4 font-medium text-lg line-height-3">{{ item.title }}</div>
           <div class="col-12 lg:col-8 text-700 line-height-3" v-html="item.text"></div>
         </div>
---
+++ components/LanguageSwitch.vue
@@ -0,0 +1,50 @@
+<script>
+import Dropdown from 'primevue/dropdown';
+
+export default {
+  name: 'LanguageSwitch',
+  components: { Dropdown },
+  data() {
+    return {
+      selected: this.$i18n.locale.toUpperCase(),
+    }
+  },
+  computed: {
+    options() {
+      return this.$i18n.locales.map(locale => locale.code.toUpperCase())
+    },
+  },
+  methods: {
+    setLocale(evt) {
+      this.$i18n.setLocale(evt.toLowerCase())
+    },
+  },
+}
+</script>
+
+<template>
+  <span class="language-switch">
+    <Dropdown v-model="selected" :options="options" @input="setLocale"/>
+  </span>
+</template>
+
+<style scoped>
+.language-switch {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+}
+
+::v-deep .p-dropdown-label.p-inputtext {
+  padding: 0.65rem;
+}
+
+::v-deep .p-dropdown .p-dropdown-trigger {
+  width: 1.5rem
+}
+
+::v-deep .p-dropdown-trigger-icon.pi.pi-chevron-down {
+  position: absolute;
+  right: 0.5rem;
+}
+</style>
